0cefacd4f7a9cfc99b3c97996a45c057668db2e7
brezzz <brezzz@users.noreply.github.com>
2020-01-01 15:46:41 -0500

    Update app.spec.js

--- test/app.spec.js
+++ test/app.spec.js
@@ -1,9 +1,10 @@
+
 const app = require('../src/app')
 
 describe('App', () => {
-  it('GET / responds with 200 containing "Hello, biolerplate!"', () => {
+  it('GET / responds with 200 containing "Hello, boilerplate!"', () => {
     return supertest(app)
       .get('/')
-      .expect(200, 'Hello, biolerplate!')
+      .expect(200, 'Hello, boilerplate!')
   })
 })
